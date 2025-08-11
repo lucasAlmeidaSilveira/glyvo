28ea882afb10a629e8522a0614e5eb3623ab5fcb
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2025-08-11 00:30:13 -0300

    add link in logo

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/Footer';
 import Logo from '@/components/Logo';
 import Menu from '@/components/Menu';
 import { Toaster } from '@/components/ui/sonner';
+import Link from "next/link";
 
 export default function MainLayout({
   children,
@@ -11,7 +12,9 @@ export default function MainLayout({
   return (
     <div className='flex flex-col items-center justify-center h-screen gap-6'>
       <div className='flex items-center justify-between gap-4'>
-        <Logo />
+        <Link href="/">
+          <Logo />
+        </Link>
         <Menu />
       </div>
       {children}
