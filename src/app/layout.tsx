06019e77102168968d9fd4ed96067fc5041ac7da
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2025-08-18 19:15:31 -0300

    fix images size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,9 +47,9 @@ export default function RootLayout({
         <head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/logo-180.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/logo-152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/logo-180.png" />
-          <link rel="apple-touch-icon" sizes="167x167" href="/logo-167.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/logo-152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/logo-180.png" />
+          <link rel="apple-touch-icon" sizes="167x167" href="/icons/logo-167.png" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="apple-mobile-web-app-title" content="Glyvo" />
